6765fb2a196103c0d08ae0526b2dcbf634ecb0cc
Miguel Hernandez <miguelhernandez.icii@gmail.com>
2011-06-28 17:40:46 -0400

    Onclick="mostrar(<?php echo $pregunta["id_pregunta"];?>)" se lo quite en cacsa 2 por el mensaje que mostraba

--- apps/frontend/modules/CACSA/templates/showPart2CACSASuccess.php
+++ apps/frontend/modules/CACSA/templates/showPart2CACSASuccess.php
@@ -8,24 +8,19 @@
   <div id="dialogo-instrucciones" style="display: none;" title="Instrucciones CACSA">
 	<p>
 		<span class="ui-icon-info-grande" style="float:left; margin:4px 4px 4px 4px;"></span>
-      En este formulario te presentamos una serie de frases que los jóvenes usan para describirse a sí mismos. Se presentan aquí para ayudarte a describir tus sentimientos y actitudes.
-      <br><br>
-      <b>Al responder trata de hacerlo del modo más serio y honesto posible.</b>
-      No te preocupes si algunas frases te parecen poco comunes, divertidas o extrañas; se han incluido para describir diferentes tipos de problemas que los jóvenes pueden tener.
-      <br><br>
-      1.- Cuando decidas que la frase <b>describe tu situación personal</b>, o <b>estás de acuerdo</b> selecciona la letra <b>V</b> para indicar que es <b>V</b>erdadera.
-      <br>
-      2.- Si decides que la frase <b>no describe tu situación personal</b>, o <b>estás en desacuerdo</b> selecciona la letra <b>F</b> para indicar que es <b>F</b>alsa.
-      <br>
-      <?php echo image_tag('ejemplorespuesta.png', 'alt=Selecciona el radio que tenga el label V o el label F') ?>
+     A continuación se presenta una lista de frases acerca de conductas y actitudes que suele tener la gente. Lee cada frase y decide si su contenido te representa a ti, o no.   <br><br>
+   
+	  <ul>
+      <li>Si la frase representa lo que tú haces o piensas, elige <b>Verdadero</b></li>
+      <li>Si la frase <b>NO</b> representa lo que tú haces o piensas, elige  <b>Falso</b></li>  
+	  </ul>
+	  <br>
+	  <b>Ejemplo</b>
+	  <br>
+	  <br>
+      <?php echo image_tag('cacsa2.jpg', 'alt=Selecciona el radio que tenga el label V o el label F') ?>
 	</p>
-  <h6>Recuerda:</h6>
-  <ul>
-    <li>Comienza leyendo cada frase, una a una y trata de responder a todas, aun cuando no estés muy seguro/a de tu decisión. Si lo has intentado pero no puedes decidirte, entonces responde la frase escribiendo la letra <b>F (Falso)</b>.</li>
-    <li>Si te equivocas o deseas cambiar alguna respuesta, selecciona nuevamente la letra correspondiente.</li>
-    <li>Si tienes dudas o deseas que te expliquen una palabra o frase, <b>pide ayuda</b>.</li>
-    <li>Para volver a hacer aparecer estas instrucciones haz click en el ícono superior <?php echo image_tag('pediarayuda.png', 'alt=el icono que tiene la i') ?>.</li>
- </ul>
+  
  <br>
   <input type="checkbox" id="LeiLasInstrucciones" name="LeiLasInstrucciones" value="LeiLasInstrucciones" /> <b>Declaro haber leido las instrucciones y comprenderlas.</b> <br>
   </div>
@@ -49,8 +44,8 @@
 		</div>
     <div class="grid-4-12">
       <ul class="form-list-rdo">
-        <li><input class="botonradio" id="<?php echo $pregunta["id_pregunta"];?>" name="radio_VorF_CACSA_<?php echo $pregunta["id_pregunta"];?>" type="radio" value="V" Onclick="mostrar(<?php echo $pregunta["id_pregunta"];?>)"/><label class="form-lbl">Verdadero</label></li>
-        <li><input class="botonradio" id="<?php echo $pregunta["id_pregunta"];?>" name="radio_VorF_CACSA_<?php echo $pregunta["id_pregunta"];?>" type="radio" value="F" Onclick="mostrar(<?php echo $pregunta["id_pregunta"];?>)"/><label class="form-lbl">Falso</label></li>
+        <li><input class="botonradio" id="<?php echo $pregunta["id_pregunta"];?>" name="radio_VorF_CACSA_<?php echo $pregunta["id_pregunta"];?>" type="radio" value="V" /><label class="form-lbl">Verdadero</label></li>
+        <li><input class="botonradio" id="<?php echo $pregunta["id_pregunta"];?>" name="radio_VorF_CACSA_<?php echo $pregunta["id_pregunta"];?>" type="radio" value="F" /><label class="form-lbl">Falso</label></li>
 			</ul>
 
     </div>
