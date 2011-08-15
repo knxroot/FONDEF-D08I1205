1c7ae6481e36543813727bcec9b5711d570720d8
gustavo@lacosox.org <nataly01>
2011-08-15 16:20:05 -0300

    Arreglando problema con boton  etc en EDA

--- apps/frontend/modules/EDA/templates/_preguntas.php
+++ apps/frontend/modules/EDA/templates/_preguntas.php
@@ -25,6 +25,7 @@
             <input type="checkbox" 
                 name="<?php echo $inputprefix ?>-F<?php echo $indice + 1 ?>C<?php echo $aux ?>"
                 value="<?php echo ($index + 1) % 2 ? 'me-identifico':'me-dicen' ?>" />
+            
         </td>
         <?php $aux=$aux+1; ?>
         <?php endforeach ?>
